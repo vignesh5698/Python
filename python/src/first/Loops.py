7600a547790edbf0f4b780619de544784a1aeac6
vignesh5698 <vigneshit13@gmail.com>
2017-08-25 12:34:08 +0530

    Example for Loops

---
+++ python/src/first/Loops.py
@@ -0,0 +1,16 @@
+'''
+Created on Aug 25, 2017
+
+@author: Vignesh
+'''
+num=int(input("Enter the Number:"))
+if num%2==0:
+    print("The Number is Even number")
+else:
+    print("The Number is Odd")
+n1=0
+while n1<num:
+    print(n1+1)
+    n1=n1+1
+
+    
