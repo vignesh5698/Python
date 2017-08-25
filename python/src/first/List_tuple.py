ab4f8ae6b46eacaadeb7867f20f9cea0f7daf75f
vignesh5698 <vigneshit13@gmail.com>
2017-08-25 06:36:24 +0530

    List and Tuple Example

---
+++ python/src/first/List_tuple.py
@@ -0,0 +1,19 @@
+'''
+Created on Aug 25, 2017
+
+@author: Vignesh
+'''
+#List
+list=[10,20.55,"List",'example',"Value can be Channged",10e07,"best"]
+print(list)
+print(list[4:5])
+print(list[2:5])
+print(list*2)
+list1=["Another list"]
+print(list+list1)
+#tuple
+tuple=(10,55.58,"Tuple example")
+print(tuple)
+tuple1=('Value',10)
+print(tuple+tuple1)
+t=("test")
