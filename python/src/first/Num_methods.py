90c2a69fc64756efd2aea99812fb63a7b21f4185
vignesh5698 <vigneshit13@gmail.com>
2017-08-26 23:12:56 +0530

    Example for Number Methods

---
+++ python/src/first/Num_methods.py
@@ -0,0 +1,17 @@
+'''
+Created on Aug 26, 2017
+
+@author: Vignesh
+'''
+import math
+x=56.98
+print(abs(x))
+print(math.ceil(x))
+print(math.exp(x))
+print(math.floor(x))
+print(math.log(x))
+li=(10,25,9,55,288)
+print(min(li))
+print(math.modf(x))
+print(math.pow(x, 2))
+
