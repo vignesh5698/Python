4e8e303c71efc8834f7560bc19c0e8e586c3ea17
vignesh5698 <vigneshit13@gmail.com>
2017-08-28 01:39:22 +0530

    Simple Function example

---
+++ python/src/first/function1.py
@@ -0,0 +1,10 @@
+'''
+Created on Aug 28, 2017
+
+@author: Vignesh
+'''
+def first(str):
+    "The First line of function has no value"
+    print(str)
+    return
+first("This is the First Function Program")        
