e38939db299ff3d1050736adaa33844bc42e0209
vignesh5698 <vigneshit13@gmail.com>
2017-08-31 13:07:20 +0530

    Example for Directory Module

---
+++ python/src/first/dir_module.py
@@ -0,0 +1,12 @@
+'''
+Created on Aug 30, 2017
+
+@author: Vignesh
+'''
+
+import math
+import random
+ran=dir(random)
+mat=dir(math)
+print("Random directory files are:",ran)
+print("Math dirctory files are:",mat)
