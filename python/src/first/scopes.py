a4dc9dc2c7de6141d54e6aba3daf2d17ca8e6851
vignesh5698 <vigneshit13@gmail.com>
2017-08-31 13:08:12 +0530

    Example for local and global scope

---
+++ python/src/first/scopes.py
@@ -0,0 +1,13 @@
+'''
+Created on Aug 30, 2017
+
+@author: Vignesh
+'''
+Money = 2000
+def AddMoney():
+# Uncomment the following line to fix the code:
+ global Money
+ Money= Money + 1
+print (Money)
+AddMoney()
+print (Money)
