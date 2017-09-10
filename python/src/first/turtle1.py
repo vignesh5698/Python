b3f71b48fecf278f5a1aadc20e1f48ea03e82e7e
vignesh5698 <vigneshit13@gmail.com>
2017-09-10 09:07:04 +0530

    Example for Turtle Graphics

---
+++ python/src/first/turtle1.py
@@ -0,0 +1,30 @@
+'''
+Created on Sep 9, 2017
+
+@author: Vignesh
+'''
+import turtle 
+import time
+win=turtle.Screen()
+pen=turtle.Turtle()
+#for i in range(4,6): 
+ #   pen.forward(100)
+   # pen.right(45)
+    #pen.forward(100)
+    #pen.right(45)
+    #pen.forward(100)
+    #pen.right(45)
+    #pen.forward(100)
+    #pen.right(45)
+   
+#for i in range(360):
+#    pen.forward(1)
+#    pen.right(1)
+
+i=0
+while True:
+    i=i+1
+    if i<360:
+     pen.forward(1)
+     pen.right(1)
+    
